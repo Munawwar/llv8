eac8f3a750aa12a06c12b3882a68dc32ede5cfa5
Vladislav Ivanishin <vlad@ispras.ru>
2015-02-09 21:09:22 +0300

    Add copyright (ISP RAS)

--- src/low-chunk.cc
+++ src/low-chunk.cc
@@ -1,3 +1,7 @@
+// Copyright 2015 ISP RAS. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "low-chunk.h"
 
 namespace v8 {
